ab70a752f688fa1604e31990adb45b8f0d40f1c4
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-09-13 22:25:21 +0900

    出勤アクションのデバック

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -22,15 +22,27 @@ class TimeController extends Controller
 
         $oldDay = '';
 
+        //退勤前に出勤を2度押せない制御
         if($oldtimein) {
             $oldTimePunchIn = new Carbon($oldtimein->punchIn);
             $oldDay = $oldTimePunchIn->startOfDay();//最後に登録したpunchInの時刻を00:00:00で代入
         }
         $today = Carbon::today();//当日の日時を00:00:00で代入
 
-        if($oldDay == $today && empty($oldtimein->punchOut)) {
+        if(($oldDay == $today) && (empty($oldtimein->punchOut))) {
             return redirect()->back();
         }
+
+        //退勤後に再度出勤を押せない制御
+        if($oldtimein) {
+            $oldTimePunchOut = new Carbon($oldtimein->punchOut);
+            $oldDay = $oldTimePunchOut->startOfDay();//最後に登録したpunchInの時刻を00:00:00で代入
+        }
+
+        if(($oldDay == $today)) {
+            return redirect()->back();
+        }
+
 
         $time = Time::create([
             'user_id' => $user->id,
